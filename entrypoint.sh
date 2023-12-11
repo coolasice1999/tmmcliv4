16a1876892cea08f42cf68d5769b0948423784cc
coolasice1999 <52601736+coolasice1999@users.noreply.github.com>
2023-12-11 09:36:43 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+# Make sure mandatory directories exist.
+mkdir -p /config/logs
+
+if [ ! -f /config/tmm.jar ]; then
+    cp -r /defaults/* /config/
+    cd /config
+    tar zxvf /config/tmm.tar.gz
+fi
+
+# Take ownership of the config directory content.
+chown -R $USER_ID:$GROUP_ID /config/*
+
 if [ ! -f /mnt/cron.conf ]; then
 #set time to 12:30AM run for movies and 12:45AM run for tvshows
     echo "30 0 * * * /config/tinyMediaManager.sh movie -u --scrapeUnscraped\n45 0 * * * /config/tinyMediaManager.sh tvshow -u --scrapeUnscraped\n" > /mnt/cron.conf
